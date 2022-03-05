444894e62ffa8d517ae434dec34e7d61460ce482
joao <programador09@speedgov.com.br>
2022-03-05 11:55:06 -0300

    finalizado o trabalho com os relatorios feitos em csv

--- application.rb
+++ application.rb
@@ -9,11 +9,32 @@ dados_dengue = formata_arquivo_para_array(arquivo_dengue)
 dados_onibus = formata_arquivo_para_array(arquivo_onibus)
 dados_alunos = formata_arquivo_para_array(arquivo_alunos)
 
-puts relatorio_alunos_sem_dengue(dados_alunos, dados_dengue).size
-puts relatorio_dengue_sem_onibus(dados_dengue, dados_onibus).size
+relatorio1 = relatorio_alunos_sem_dengue(dados_alunos, dados_dengue)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Genero", "Data de Nascimento"], "relatorio1.csv", relatorio1)
 
-puts relatorio_onibus_sem_dengue(dados_onibus, dados_dengue).size
-puts relatorio_alunos_com_dengue(dados_alunos, dados_dengue).size
+relatorio2 = relatorio_dengue_sem_onibus(dados_dengue, dados_onibus)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Sexo", "Data de Nascimento", "Data da dengue"], "relatorio2.csv", relatorio2)
 
-puts relatorio_alunos_que_utilizam_onibus(dados_onibus, dados_alunos).size
-puts relatorio_dengue_com_onibus(dados_dengue, dados_onibus).size
+relatorio3 = relatorio_onibus_sem_dengue(dados_onibus, dados_dengue)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Genero", "Data de Nascimento", "onibus"], "relatorio3.csv", relatorio3)
+
+relatorio4 = relatorio_alunos_com_dengue(dados_alunos, dados_dengue)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Genero", "Data de Nascimento"], "relatorio4.csv", relatorio4)
+
+relatorio5 = relatorio_alunos_que_utilizam_onibus(dados_onibus, dados_alunos)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Genero", "Data de Nascimento", "onibus"], "relatorio5.csv", relatorio5)
+
+relatorio6 = relatorio_dengue_com_onibus(dados_dengue, dados_onibus)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Sexo", "Data de Nascimento", "Data da dengue"], "relatorio6.csv", relatorio6)
+
+relatorio7 = relatorio_aluno_com_dengue_e_com_onibus(dados_alunos, dados_dengue, dados_onibus)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Genero", "Data de Nascimento"], "relatorio7.csv", relatorio7)
+
+relatorio8 = relatorio_dengue_sem_onibus(dados_dengue, dados_onibus)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Sexo", "Data de Nascimento", "Data da dengue"], "relatorio8.csv", relatorio8)
+
+relatorio9 = relatorio_dengue_sem_alunos(dados_dengue,dados_alunos)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Sexo", "Data de Nascimento", "Data da dengue"], "relatorio9.csv", relatorio9)
+
+relatorio10 = relatorio_dengue_sem_escola_sem_onibus(dados_dengue, dados_alunos, dados_onibus)
+cria_csv_com_cabecalhos(["id", "Nome", "Nome pai", "Nome mãe", "Sexo", "Data de Nascimento", "Data da dengue"], "relatorio10.csv", relatorio10)
--- functions.rb
+++ functions.rb
@@ -9,18 +9,40 @@ def formata_arquivo_para_array(arquivo)
   return dados
 end
 
+def cria_csv_com_cabecalhos(cabecalhos, arquivo, dados)
+  CSV.open(arquivo, "w") do |csv|
+    csv.to_io.write "\uFEFF"
+    csv << cabecalhos
+    dados.each do |dado|
+      #puts dado
+      csv << dado
+    end
+  end
+end
+
 def compara_strings_formatadas(string1, string2)
   string1_formatado = string1.split(" ")
   string2_formatado = string2.split(" ")
+  #Separa cada nome da pessoa em um array
+  return true if string1_formatado[1] == string2_formatado[1] && string1_formatado[2] == string2_formatado[2]
+  # compara se os sobrenomes são os mesmos
 
-  if string1.size == string2.size
-    if string1 == string2
-      string1_formatado.each_with_index do |string, index|
-        return false if string1_formatado[ index ][ index ] != string2_formatado[ index ][ index ]
+  if string1 != string2 && string1_formatado.size == string2_formatado.size
+    string1_formatado.each_with_index do |string, index|
+      nome1 = string1_formatado[ index ]
+      nome2 = string2_formatado[ index ]
+      #pego o nome de cada pessoa
+
+      if nome1 != nil && nome2 != nil
+        return false if nome1[ index ] != nome2[ index ]
+        #compara a inicial de cada nome
+      else
+        return false
       end
-      return true
     end
+    return true
   end
+
   return string1 == string2
 end
 
@@ -61,43 +83,46 @@ def loop_de_comparacao_dupla_de_dados(dados_principais, dados_comparativos, loop
       dado_formatado << dado_principal
     end
   end
+  return dado_formatado
+end
 
-  def loop_de_comparacao_tripla_de_dados(dados_principais, dados_secundarios ,dados_terciarios ,loop_de_exclusao_secundario, loop_de_exclusao_terciario)
-    dado_formatado = Array.new
+def loop_de_comparacao_tripla_de_dados(dados_principais, dados_secundarios ,dados_terciarios ,loop_de_exclusao_secundario, loop_de_exclusao_terciario)
+  dado_formatado = Array.new
 
-    dados_principais.each do |dado_principal|
-      condicao_secundaria_atendida, condicao_terciaria_atendidada = false
+  dados_principais.each do |dado_principal|
+    condicao_secundaria_atendida = false
+    condicao_terciaria_atendidada = false
 
-      dado_secundario_formatado,dado_terciario_formatado = nil
+    dado_secundario_formatado = nil
+    dado_terciario_formatado = nil
 
-      dados_secundarios.each do |dado_secundario|
-        if eh_a_mesma_pessoa?( dado_principal[1], dado_secundario[1], dado_principal[3], dado_secundario[3], dado_principal[2], dado_secundario[2] )
-          if loop_de_exclusao_secundario
-            condicao_secundaria_atendida = true
-            break
-          else
-            dado_secundario_formatado = dado_secundario
-          end
+    dados_secundarios.each do |dado_secundario|
+      if eh_a_mesma_pessoa?( dado_principal[1], dado_secundario[1], dado_principal[3], dado_secundario[3], dado_principal[2], dado_secundario[2] )
+        if loop_de_exclusao_secundario
+          condicao_secundaria_atendida = true
+          break
+        else
+          dado_secundario_formatado = dado_secundario
         end
-      end
-
-      dados_terciarios.each do |dado_terciario|
-        if eh_a_mesma_pessoa?( dado_principal[1], dado_terciario[1], dado_principal[3], dado_terciario[3], dado_principal[2], dado_terciario[2] )
-          if loop_de_exclusao_terciario
-            condicao_secundaria_atendida = true
-            break
-          else
-            dado_terciario_formatado = dado_terciario
-          end
-        end
-      end
-
-      if dado_secundario_formatado != nil && dado_terciario_formatado != nil
-        dado_formatado << dado_principal
       end
     end
-  end
 
+    dados_terciarios.each do |dado_terciario|
+      if eh_a_mesma_pessoa?( dado_principal[1], dado_terciario[1], dado_principal[3], dado_terciario[3], dado_principal[2], dado_terciario[2] )
+        if loop_de_exclusao_terciario
+          condicao_terciaria_atendidada = true
+          break
+        else
+          dado_terciario_formatado = dado_terciario
+        end
+      end
+    end
+    if dado_secundario_formatado != nil && dado_terciario_formatado != nil
+      dado_formatado << dado_principal
+    elsif loop_de_exclusao_secundario && loop_de_exclusao_terciario && condicao_secundaria_atendida == false && condicao_terciaria_atendidada == false
+      dado_formatado << dado_principal
+    end
+  end
   return dado_formatado
 end
 
@@ -123,6 +148,18 @@ end
 
 def relatorio_dengue_com_onibus(dados_dengue, dados_onibus)
   loop_de_comparacao_dupla_de_dados(dados_dengue, dados_onibus, false)
+end
+
+def relatorio_aluno_com_dengue_e_com_onibus(dados_alunos, dados_dengue, dados_onibus)
+  loop_de_comparacao_tripla_de_dados(dados_alunos, dados_dengue, dados_onibus, false , false)
+end
+
+def relatorio_dengue_sem_escola_sem_onibus(dados_dengue, dados_alunos, dados_onibus)
+  loop_de_comparacao_tripla_de_dados(dados_dengue, dados_alunos, dados_onibus, true , true)
+end
+
+def relatorio_dengue_sem_alunos(dados_dengue,dados_alunos)
+  loop_de_comparacao_dupla_de_dados(dados_dengue, dados_alunos, true)
 end
 
 
